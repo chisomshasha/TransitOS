f7d4ba8e52368df796c5d7e0fb749fdfa5ee2436
chisomshasha <chisomarinzeshasha@gmail.com>
2026-09-05 11:05:05 +0100

    Update [id].tsx

--- frontend/app/(app)/trips/[id].tsx
+++ frontend/app/(app)/trips/[id].tsx
@@ -3,7 +3,7 @@ import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import * as Print from 'expo-print';
 import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import {
   AlertTriangle,
   ClipboardList,
